25cb158359a3115742b9c4fae5c1d25df6d36815
Penglyn <epicender56@gmail.com>
2022-02-13 20:49:16 +0000

    Initial release

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+cd ~
 sudo dnf upgrade -y
 flatpak update -y
+sudo dnf install zsh zsh-autosuggestions zsh-syntax-hylighting util-linux-user -y
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+wget https://raw.githubusercontent.com/Penglyn/dotfiles/main/.zshrc
+sudo chsh $USER -s /bin/zsh
